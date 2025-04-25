c76bbd7338cb59e739cd10a2363364159503fed9
BostonBoomBringer <l.olartec@uniandes.edu.co>
2025-04-25 01:00:10 -0500

    Update TicketAuthentication.java

--- DiseñoDPOO/src/TicketsManagement/TicketAuthentication.java
+++ DiseñoDPOO/src/TicketsManagement/TicketAuthentication.java
@@ -1,5 +1,27 @@
-package TicketsManagement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicketAuthentication {
 
-}
+    private TicketManager ticketManager;
+  
+    public TicketAuthentication(TicketManager ticketManager) {
+        this.ticketManager = ticketManager;
+    }
+
+    public boolean validate(String ticketTypeName) {
+        try {
+            TicketType type = TicketType.valueOf(ticketTypeName);
+            return type != null;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public boolean registerTicket(TicketManager ticketManagement) {
+        if (ticketManagement == null) {
+            return false;
+        }
+        this.ticketManager = ticketManagement;
+        return true;
+    }
